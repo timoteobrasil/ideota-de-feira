a120267b79600be4edfb3c5307bfd36af0cabbd1
Timóteo de Souza Brasil <timoteo.brasil@gmail.com>
2023-08-26 21:52:59 -0300

    inicializando o banco em memória com dados

--- src/main/java/br/com/timoteobrasil/ideotafeira/modelo/Produto.java
+++ src/main/java/br/com/timoteobrasil/ideotafeira/modelo/Produto.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Produtos")
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Categorias (nome, descricao) VALUES ('Categoria 1', 'Descricao da Categoria 1');
+INSERT INTO Categorias (nome, descricao) VALUES ('Categoria 2', 'Descricao da Categoria 2');
+INSERT INTO Produtos (nome, descricao, categoria_id) VALUES ('Produto 1', 'Descricao do Produto 1', 1);
+INSERT INTO Produtos (nome, descricao, categoria_id) VALUES ('Produto 2', 'Descricao do Produto 2', 1);
+INSERT INTO Produtos (nome, descricao, categoria_id) VALUES ('Produto 3', 'Descricao do Produto 3', 2);
+INSERT INTO Produtos (nome, descricao, categoria_id) VALUES ('Produto 4', 'Descricao do Produto 4', 2);
